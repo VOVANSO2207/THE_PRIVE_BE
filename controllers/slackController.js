3bb46bc536ecd0eef38c65ab017c090c6ec514b5
Vo Van So <vovanso2004@gmail.com>
2025-07-17 09:37:38 +0700

    secure WEbhook

--- controllers/slackController.js
+++ controllers/slackController.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
-
-const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
+require('dotenv').config();
+const SLACK_WEBHOOK_URL = process.env.SLACK_URL;
 
 exports.sendSlackNotification = async (req, res) => {
   const { creatorName, roomUrl } = req.body;
